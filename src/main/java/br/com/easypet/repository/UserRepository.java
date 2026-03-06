2a017c6574f0d5b0769b013461524baa3c7bef6a
randy <raandy.gc@gmail.com>
2026-03-06 17:46:59 -0300

    feat: add UserRepository

---
+++ src/main/java/br/com/easypet/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.easypet.repository;
+
+import br.com.easypet.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}
