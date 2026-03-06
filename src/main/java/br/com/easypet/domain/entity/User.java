cde1ec87f440b4392dc2eaf491ba01b163c9f00a
randy <raandy.gc@gmail.com>
2026-03-06 17:38:56 -0300

    feat: add User entity, Role enum and V1 Flyway migration

--- src/main/java/br/com/easypet/domain/entity/User.java
+++ src/main/java/br/com/easypet/domain/entity/User.java
@@ -1,4 +1,36 @@
 package br.com.easypet.domain.entity;
 
+
+import br.com.easypet.domain.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
