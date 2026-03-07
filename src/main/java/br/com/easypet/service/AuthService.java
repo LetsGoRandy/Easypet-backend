ed4718a73fadda8fbe29ee71f99dca8a6d52b68a
randy <raandy.gc@gmail.com>
2026-03-07 04:10:06 -0300

    feat: implement JWT authentication - register, login and security config

---
+++ src/main/java/br/com/easypet/service/AuthService.java
@@ -0,0 +1,57 @@
+package br.com.easypet.service;
+
+import br.com.easypet.domain.entity.User;
+import br.com.easypet.domain.enums.Role;
+import br.com.easypet.dto.request.LoginRequest;
+import br.com.easypet.dto.request.RegisterRequest;
+import br.com.easypet.dto.response.AuthResponse;
+import br.com.easypet.repository.UserRepository;
+import br.com.easypet.security.JwtService;
+import jdk.jshell.spi.ExecutionControl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw new RuntimeException("Email já cadastrado");
+        }
+
+        User user = User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user.getEmail());
+
+        return new AuthResponse(token, user.getName(), user.getEmail(), user.getRole().name());
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(), request.password())
+        );
+
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(()->new RuntimeException("Usuário não encontrado"));
+
+        String token = jwtService.generateToken(user.getEmail());
+
+        return new AuthResponse(token, user.getName(), user.getEmail(), user.getRole().name());
+    }
+}
